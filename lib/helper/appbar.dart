b030a50ec92e2772ebbaffceb8467ea3c21825a3
ali <bbbeat114@gmail.com>
2023-05-11 13:20:40 +0300

    main

--- lib/helper/appbar.dart
+++ lib/helper/appbar.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_to_airplay/flutter_to_airplay.dart';
 import 'package:provider/provider.dart';
 
 import '../controlller/user_controller.dart';
@@ -7,21 +10,23 @@ import '../controlller/user_controller.dart';
 class MyAppBar extends AppBar {
   final String titleText;
 
-  MyAppBar(context,{required this.titleText})
+  MyAppBar(context, {required this.titleText})
       : super(
             backgroundColor: Colors.transparent,
             elevation: 0,
             title: Row(
               children: [
                 GestureDetector(
-                  onTap: (){
-                    if ( Provider.of<UserController>(context, listen: false).user == null) {
-                    Navigator.of(context).pushNamed('/login');
-                      
-                    }else{
-                    Navigator.of(context).pushNamed('/profile');}
-                  },
-                  child: SvgPicture.asset('assets/images/person.svg')),
+                    onTap: () {
+                      if (Provider.of<UserController>(context, listen: false)
+                              .user ==
+                          null) {
+                        Navigator.of(context).pushNamed('/login');
+                      } else {
+                        Navigator.of(context).pushNamed('/profile');
+                      }
+                    },
+                    child: SvgPicture.asset('assets/images/person.svg')),
                 Container(
                   width: 20,
                 ),
@@ -29,7 +34,14 @@ class MyAppBar extends AppBar {
                 Container(
                   width: 20,
                 ),
-                SvgPicture.asset('assets/images/screen.svg')
+                if (Platform.isIOS)
+                  AirPlayRoutePickerView(
+                    tintColor: Colors.white,
+                    activeTintColor: Colors.white,
+                    backgroundColor: Colors.transparent,
+                    onClosePickerView: () {},
+                    onShowPickerView: () {},
+                  ),
               ],
             ),
             actions: [
